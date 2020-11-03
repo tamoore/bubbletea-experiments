79a8e34202988a31c6c3c84971aa21a32dab5dcd
Todd Moore <11851685+tamoore@users.noreply.github.com>
2020-11-03 04:54:29 +0000

    Add commands breakdown

--- main.go
+++ main.go
@@ -2,109 +2,99 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+const url = "https://charm.sh/"
+
+type statusMsg int
+
+type errMsg struct{ err error }
+
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+	status int
+	err    error
 }
 
-var initialModel = model{
-	// Our to-do list is just a grocery list
-	choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+func checkServer() tea.Msg {
 
-	// A map which indicates which choices are selected. We're using
-	// the  map like a mathematical set. The keys refer to the indexes
-	// of the `choices` slice, above.
-	selected: make(map[int]struct{}),
+	// Create an HTTP client and make a GET request.
+	c := &http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(url)
+
+	if err != nil {
+		// There was an error making our request. Wrap the error we received
+		// in a message and return it.
+		return errMsg{err}
+	}
+	// We received a response from the server. Return the HTTP status code
+	// as a message.
+	return statusMsg(res.StatusCode)
 }
+
+// For messages that contain errors it's often handy to also implement the
+// error interface on the message.
+func (e errMsg) Error() string { return e.err.Error() }
 
 func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
-	return nil
+	return checkServer
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
-	// Is it a key press?
+	case statusMsg:
+		// The server returned a status message. Save it to our model. Also
+		// tell the Bubble Tea runtime we want to exit because we have nothing
+		// else to do. We'll still be able to render a final view with our
+		// status message.
+		m.status = int(msg)
+		return m, tea.Quit
+
+	case errMsg:
+		// There was an error. Note it in the model. And tell the runtime
+		// we're done and want to quit.
+		m.err = msg
+		return m, tea.Quit
+
 	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
+		// Ctrl+c exits. Even with short running programs it's good to have
+		// a quit key, just incase your logic is off. Users will be very
+		// annoyed if they can't exit.
+		if msg.Type == tea.KeyCtrlC {
 			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
 		}
 	}
 
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
+	// If we happen to get any other messages, don't do anything.
 	return m, nil
 }
 
 func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	// If there's an error, print it out and don't do anything else.
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
 	}
 
-	// The footer
-	s += "\nPress q to quit.\n"
+	// Tell the user we're doing something.
+	s := fmt.Sprintf("Checking %s ... ", url)
 
-	// Send the UI for rendering
-	return s
+	// When the server responds with a status, add it to the current line.
+	if m.status > 0 {
+		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
+	}
 
+	// Send off whatever we came up with above for rendering.
+	return "\n" + s + "\n\n"
 }
 
 func main() {
-	p := tea.NewProgram(initialModel)
-	if err := p.Start(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+	if err := tea.NewProgram(model{}).Start(); err != nil {
+		fmt.Printf("Uh oh, there was an error: %v\n", err)
 		os.Exit(1)
 	}
 }
